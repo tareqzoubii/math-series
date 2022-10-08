0bd39aae7b975d947bcbcac3437484ac1513749c
tareqzoubii <tareqzoubi4@gmail.com>
2022-10-08 11:31:11 -0700

    V1.1

--- math_series/math_series.py
+++ math_series/math_series.py
@@ -1,5 +1,6 @@
 
 def Fibonacci(ele):
+    """ This function is created to return the (ele index) number in the series """
     if ele == 0:
         return 0
     elif ele == 1:
@@ -11,6 +12,7 @@ def Fibonacci(ele):
             
 
 def Lucas(ele):
+    """" This function is created to return the (ele index) number in the series """
     if ele == 0:
         return 2
     elif ele == 1:
@@ -21,6 +23,7 @@ def Lucas(ele):
         return Lucas(ele - 2) + Lucas(ele - 1)
 
 def sum_series(ele, x=0, y=0):
+    """ This function is created to allow the user to choose the parameters with him self so if the user choosse one ele it will work with the fibonacci, if the user choose three ele it will work with lucas otherwise it will create a new series """
     if ele == 0:
         return x
     elif ele == 1:
